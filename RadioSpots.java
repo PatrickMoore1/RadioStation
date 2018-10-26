f38fd3b5b4922487800513721e99c4c1bad6d5e4
PatrickMoore1 <35699967+PatrickMoore1@users.noreply.github.com>
2018-10-26 09:27:14 -0400

    RadioSpots Class
    RadioSpots are set time slots with a title and length where the radio station can play one of their own spots.

---
+++ RadioSpots.java
@@ -0,0 +1,12 @@
+
+public class RadioSpots extends Audio{
+
+	public RadioSpots(String title, int audioLength) {
+		super(title, audioLength);
+		// TODO Auto-generated constructor stub
+	}
+
+
+
+	
+}
