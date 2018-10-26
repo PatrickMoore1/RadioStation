76dd2801f5d9c698f79410e52cab536ef7ad9603
PatrickMoore1 <35699967+PatrickMoore1@users.noreply.github.com>
2018-10-26 09:26:04 -0400

    OldSongs Class
    OldSongs are songs that have a set title, author, and length, plus an additional field that will keep track of when it was last played. Old Songs must not be played more than once per month

---
+++ oldSongs.java
@@ -0,0 +1,39 @@
+
+public class oldSongs extends Audio {
+
+	private String author;
+	private String datePlayed;
+	
+	
+	public oldSongs(String title, int audioLength, String author, String datePlayed) {
+		super(title, audioLength);
+		this.author = author;
+		this.datePlayed = datePlayed;
+	}
+
+
+	public String getAuthor() {
+		return author;
+	}
+
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+
+	public String getDatePlayed() {
+		return datePlayed;
+	}
+
+
+	public void setDatePlayed(String datePlayed) {
+		this.datePlayed = datePlayed;
+	}
+	
+	
+	
+
+	}
+
+
