99a130f6b3436c42305adb504cdbe8634061d20b
PatrickMoore1 <35699967+PatrickMoore1@users.noreply.github.com>
2018-11-26 13:05:05 -0500

    Update RadioGUI.java

--- RadioGUI.java
+++ RadioGUI.java
@@ -1,8 +1,14 @@
 import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
 
 import javax.swing.*;
+
+import OrderCalculatorGUI.CalcButtonListener;
 
 public class RadioGUI extends JFrame {
 	
@@ -14,9 +20,12 @@ public class RadioGUI extends JFrame {
 	private JPanel djPanel;
 	private playlistPanel songPanel;
 	private JFrame frame;
+	private ArrayList<Audio> playlist;
+	private String currentSong;
 	
 	
-	public RadioGUI()	{
+	public RadioGUI(ArrayList<Audio> playlist)	{
+		this.playlist = playlist;
 		makeFrame();
 	}
 	
@@ -27,7 +36,7 @@ public class RadioGUI extends JFrame {
 		setLayout(new BorderLayout());
 
 		
-		songPanel = new playlistPanel();
+		songPanel = new playlistPanel(playlist);
 		picturePanel = new JPanel();
 		buttonPanel = new JPanel();
 		currentPlaying = new JPanel();
@@ -35,18 +44,61 @@ public class RadioGUI extends JFrame {
 		
 		createPlaylist = new JButton("Create Playlist");
 		buttonPanel.add(createPlaylist);
+		createPlaylist.addActionListener(new createPlaylistListener());
 		
-		currentTitle = new JLabel("Current Song");
+		currentTitle = new JLabel();
 		currentPlaying.add(currentTitle);
+		
 		
 		add(songPanel, BorderLayout.WEST);
 		add(picturePanel, BorderLayout.NORTH);
 		add(buttonPanel, BorderLayout.SOUTH);
 		add(currentPlaying, BorderLayout.CENTER);
 		
+		
+		
 		pack();
 	    setVisible(true);
 		
 	}
+	
+	
+	//private class createPlaylistListener implements ActionListener
+	 //  {
+	  //    public void actionPerformed(ActionEvent e)
+	   ///   {
+	     //    // Variables to hold the subtotal, tax, and total
+	      //   double subtotal, tax, total;
+
+	         // Calculate the subtotal.
+	         //subtotal = bagels.getBagelCost() + 
+	           //         toppings.getToppingCost() +
+	             //       coffee.getCoffeeCost();
+//
+	//         // Calculate the sales tax.
+	  //       tax = subtotal * TAX_RATE;
+//
+	//         // Calculate the total.
+	  //       total = subtotal + tax;
+//
+	//         // Create a DecimalFormat object to format output.
+	  //       DecimalFormat dollar = new DecimalFormat("0.00");
+//
+	//         // Display the charges.
+	  //       JOptionPane.showMessageDialog(null, "Subtotal: $" +
+	    //                   dollar.format(subtotal) + "\n" +
+	      //                 "Tax: $" + dollar.format(tax) + "\n" +
+	        //               "Total: $" + dollar.format(total));
+	     // }//
+	   //}//
+
+	public void setCurrentSong(String currentSong) {
+		currentTitle.setText("Now Playing: " + currentSong);
+		pack();
+	    setVisible(true);
+		
+		
+	}
 
 }
+
