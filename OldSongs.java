7daf0a03affaa77c8e62c2c537339c955a3ced09
PennNguyen <34245455+PennNguyen@users.noreply.github.com>
2018-11-28 17:57:04 -0500

    Update OldSongs.java

--- OldSongs.java
+++ OldSongs.java
@@ -1,38 +1,62 @@
-
-public class OldSongs extends Audio {
+/**
+ * Another subclass of Audio
+ * @author Kevin Nguyen
+ *
+ */
+public class oldSongs extends Audio {
 
 	private String author;
 	private String datePlayed;
-	
-	
+
+	/**
+	 * Instance of oldSong is created
+	 * @param title				Title of the song 
+	 * @param audioLength		Length of the song
+	 * @param author			The author of the song
+	 * @param datePlayed		The date of the song played
+	 */
 	public oldSongs(String title, int audioLength, String author, String datePlayed) {
 		super(title, audioLength);
 		this.author = author;
 		this.datePlayed = datePlayed;
 	}
 
-
+	/**
+	 * Gives back the author name
+	 * @return		The authors name
+	 */
 	public String getAuthor() {
 		return author;
 	}
 
-
+	/**
+	 * Sets the author name to the input
+	 * @param author		The name of the author
+	 */
 	public void setAuthor(String author) {
 		this.author = author;
 	}
 
-
+	/**
+	 * Gives back the date played
+	 * 
+	 * @return		The date played
+	 */
 	public String getDatePlayed() {
 		return datePlayed;
 	}
 
-
+	/**
+	 * Sets the date played of the song
+	 * @param datePlayed		The date that the song played
+	 */
 	public void setDatePlayed(String datePlayed) {
 		this.datePlayed = datePlayed;
 	}
-	
 
 
-	}
+}
+
+
 
 
