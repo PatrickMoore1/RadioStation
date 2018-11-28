a7a8c9a1a686c011e376997a06bfd42ec2e58275
PatrickMoore1 <35699967+PatrickMoore1@users.noreply.github.com>
2018-11-28 16:40:25 -0500

    Update RadioGUI.java

--- RadioGUI.java
+++ RadioGUI.java
@@ -11,12 +11,12 @@ import javax.swing.*;
 
 
 public class RadioGUI extends JFrame {
-	
+
 	private JButton createPlaylist;
 	private JPanel picturePanel;
 	private JPanel buttonPanel;
 	private JPanel currentPlaying;
-	private JLabel currentTitle;
+	private static JLabel currentTitle;
 	private JPanel djPanel;
 	private playlistPanel songPanel;
 	private JFrame frame;
@@ -24,70 +24,71 @@ public class RadioGUI extends JFrame {
 	private ArrayList<Audio> playlist;
 	private String currentSong;
 	private boolean isAction = false;
-	
+
 
 	public RadioGUI(ArrayList<Audio> playlist)	{
 		this.playlist = playlist;
 		makeFrame();
+		displayList(playlist);
 	}
-	
+
 	public void makeFrame()	{
-		
+
 		setTitle("Radio Station");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLayout(new BorderLayout());
 
-		
 		songPanel = new playlistPanel(playlist);
 		picturePanel = new JPanel();
 		buttonPanel = new JPanel();
 		currentPlaying = new JPanel();
-			
+
 		createPlaylist = new JButton("Create Playlist");
 		buttonPanel.add(createPlaylist);
 		createPlaylist.addActionListener(new createPlaylistListener());
-		
+
 		currentTitle = new JLabel();
 		currentPlaying.add(currentTitle);
 		scrollPane = new JScrollPane(songPanel);
-			
+
 		add(scrollPane, BorderLayout.WEST);
 		add(picturePanel, BorderLayout.NORTH);
 		add(buttonPanel, BorderLayout.SOUTH);
 		add(currentPlaying, BorderLayout.CENTER);
-		
+
 		pack();
-	    setVisible(true);
-		
+		setVisible(true);
 	}
-	
+
 	private class createPlaylistListener implements ActionListener
-	   {
-	      public void actionPerformed(ActionEvent e)
-	      {
-	    	  isAction = true;
-
-	      }
-	   }
-
-
-	public void setCurrentSong(String currentSong) {
-		currentTitle.setText("Now Playing: " + currentSong);
-		pack();
-	    setVisible(true);
-		
-		
-	}
-	
-	
-	public boolean getIsAction() {
-		return isAction;
+	{
+		public void actionPerformed(ActionEvent e)
+		{
+			isAction = true;
+		}
 	}
 
-	public void setAction(boolean isAction) {
-		this.isAction = isAction;
+	public static void setCurrentSong(String currentSong) {
+		currentTitle.setText("Now Playing: " + currentSong);	
 	}
 
+	public static void displayList(ArrayList<Audio> audioList)	{
+
+		for(Audio audio : audioList)	{
+			System.out.print("Title: " + audio.getTitle() + ". ");				 
+			System.out.println("Audio Length:" + audio.getAudioLength());
+			delay(audio.getAudioLength() * 200);
+			setCurrentSong(audio.getTitle());
+		}		
+	}
+
+	public static void delay(int milliseconds)	{
+		try {
+			Thread.sleep(milliseconds);
+		}
+		catch (InterruptedException ie)	{
+		}		
+	}
 }
 
 
