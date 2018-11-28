e0433465247a3acd3019ef07d2dc39e698bf45d4
PennNguyen <34245455+PennNguyen@users.noreply.github.com>
2018-11-28 17:51:07 -0500

    Update RadioSpots.java

--- RadioSpots.java
+++ RadioSpots.java
@@ -1,6 +1,16 @@
-
+/**
+ * Another subclass of Audio 
+ * 
+ * @author Kevin Nguyen
+ *
+ */
 public class RadioSpots extends Audio{
 
+	/**
+	 * Instance of RadioSpots is created
+	 * @param title			Title of the radio spots
+	 * @param audioLength	The length of the radioSpot
+	 */
 	public RadioSpots(String title, int audioLength) {
 		super(title, audioLength);
 		// TODO Auto-generated constructor stub
