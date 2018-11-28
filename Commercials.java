f06a01abaa196d54dd74a2f2ba35bcf034747655
PennNguyen <34245455+PennNguyen@users.noreply.github.com>
2018-11-28 18:17:13 -0500

    Update Commercials.java

--- Commercials.java
+++ Commercials.java
@@ -1,10 +1,17 @@
-
+/**
+ * Another subclass of Audio
+ * @author Kevin Nguyen
+ *
+ */
 public class Commercials extends Audio {
 
+	/**
+	 * The commercial is created
+	 * @param title				The name of the commercial
+	 * @param audioLength		The length of the audiolength
+	 */
 	public Commercials(String title, int audioLength) {
 		super(title, audioLength);
 		
 	}
-	
-
 }
