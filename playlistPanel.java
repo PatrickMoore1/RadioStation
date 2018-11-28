dc90141009c2e8ae1a3c757c14ea7494131bbb6c
PatrickMoore1 <35699967+PatrickMoore1@users.noreply.github.com>
2018-11-28 16:41:33 -0500

    Update playlistPanel.java

--- playlistPanel.java
+++ playlistPanel.java
@@ -1,61 +1,19 @@
 import java.awt.GridLayout;
-import javax.swing.*;
-
-import java.awt.*;
-import java.awt.event.*;
-import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class playlistPanel extends JPanel{
-	
-	private JLabel title1;
-	private JLabel title2;
-	private JLabel title3;
-	private JLabel title4;
-	private JLabel title5;
-	private JLabel title6;
-	private JLabel title7;
-	private JLabel title8;
-	private JLabel title9;
-	
+
+	private ArrayList<Audio> playlist;
+
 	public playlistPanel(ArrayList<Audio> playlist)	{
-		setLayout(new GridLayout(9,1));
-		
-		title1 = new JLabel("title1");
-		title2 = new JLabel("title2");
-		title3 = new JLabel("title3");
-		title4 = new JLabel("title4");
-		title5 = new JLabel("title5");
-		title6 = new JLabel("title6");
-		title7 = new JLabel("title7");
-		title8 = new JLabel("title8");
-		title9 = new JLabel("title9");
-		
-		add(title1);
-		add(title2);
-		add(title3);
-		add(title4);
-		add(title5);
-		add(title6);
-		add(title7);
-		add(title8);
-		add(title9);
-	
-	//public playlistPanel(ArrayList<Audio> playlist)	{
-		//setLayout(new GridLayout(9,1));
-		//this.playlist = playlist;
-			
-			//for(int count = 0; count <= playlist.size(); count++) {
-				//add(new JLabel(playlist.get(count).getTitle()));
-			//}		
-		//}
-	
-//	public void clear()	{
-		//for(int count = 0; count <= playlist.size(); count++) {
-		//add(new JLabel(""));
-	//}
+		setLayout(new GridLayout(playlist.size(),1));
+		this.playlist = playlist;
+
+		for(int count = 0; count < playlist.size(); count++) {
+			add(new JLabel(playlist.get(count).getTitle()));
+		}		
 	}
 }
