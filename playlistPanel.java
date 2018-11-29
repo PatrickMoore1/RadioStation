98841121a5d5776030f972cf5bcd0deca356d8b8
PatrickMoore1 <35699967+PatrickMoore1@users.noreply.github.com>
2018-11-28 19:59:09 -0500

    Update playlistPanel.java

--- playlistPanel.java
+++ playlistPanel.java
@@ -1,14 +1,9 @@
-import java.awt.*;
 import java.awt.GridLayout;
 import java.util.ArrayList;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-/**
- * The panel for the playlist is created with the needed information
- * @author Patrick Moore
- *
- */
+
 public class playlistPanel extends JPanel{
 
 	private ArrayList<Audio> playlist;
@@ -19,7 +14,10 @@ public class playlistPanel extends JPanel{
 
 		for(int count = 0; count < playlist.size(); count++) {
 			add(new JLabel(playlist.get(count).getTitle()));
-		}		
+		}	
+		
+	//	for(JLabel song : songPanel)
+		
 	}
 }
 
