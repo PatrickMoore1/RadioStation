dedee63e3a89b3cc09329fad99436072d524bad8
PatrickMoore1 <35699967+PatrickMoore1@users.noreply.github.com>
2018-11-29 22:32:14 -0500

    Update RadioGUI.java

--- RadioGUI.java
+++ RadioGUI.java
@@ -1,118 +1,114 @@
 import java.awt.BorderLayout;
-import java.awt.Container;
-import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 import javax.swing.*;
 
-
-
 public class RadioGUI extends JFrame {
-
+	
+	private ArrayList<Audio> playlist = new ArrayList<Audio>();
+	private ImageIcon image = new ImageIcon("radio.jpg");
+	private DJS djs = new DJS();
+	
+	private JScrollPane scrollPane;
 	private JButton createPlaylist;
+	private JPanel currentPlaying;
 	private JPanel picturePanel;
 	private JPanel buttonPanel;
-	private JPanel currentPlaying;
-	private JLabel currentTitle;
 	private JPanel djPanel;
-	private playlistPanel songPanel;
-	private JFrame frame;
-	private JScrollPane scrollPane;
-	ArrayList<Audio> playlist = new ArrayList<Audio>();
-	private String currentSong;
-	private boolean isAction = false;
-	private RadioStations wmmr = new RadioStations();
+	private JLabel currentTitle;
 	private JLabel currentDJ;
-	private DJS djs = new DJS();
-	//private boolean isPlaying = false;
+	private JLabel picture;
+	private JFrame frame;
 	
-
+	private playlistPanel songPanel;
+	private String currentSong;
 
 	public RadioGUI()	{	
 		makeGUI();
 	}
-	
+
 	public void makeGUI()	{
-		
+
 		RadioStations wmmr = new RadioStations();
-		wmmr.sortMasterList();
+	    wmmr.sortMasterList();
 		wmmr.createPlaylist();
-		
+
 		playlist = wmmr.getDjSet1();
-		
+	
+		displayList(playlist);
 		makeFrame();
-		displayList(playlist); 
 	}
 
 
 	public void makeFrame()	{
 
-		setTitle("Radio Station");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setLayout(new BorderLayout());
+		frame = new JFrame();
+		frame.setTitle("Radio Station");
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		frame.setLayout(new BorderLayout());
 
 		songPanel = new playlistPanel(playlist);
+		
+		currentPlaying = new JPanel();
 		picturePanel = new JPanel();
 		buttonPanel = new JPanel();
-		currentPlaying = new JPanel();
+		djPanel = new JPanel();
+		
+		picture = new JLabel(image);
+		currentDJ = new JLabel();
 
 		createPlaylist = new JButton("Create Playlist");
 		buttonPanel.add(createPlaylist);
+		
 		createPlaylist.addActionListener(new createPlaylistListener());
+		
+		picturePanel.add(picture);
 
 		currentTitle = new JLabel();
-		currentPlaying.add(currentTitle);
 		scrollPane = new JScrollPane(songPanel);
-		
-		djPanel = new JPanel();
-		currentDJ = new JLabel();
-		djPanel.add(currentDJ);
-		currentDJ.setText(djs.getDjName());
-		
-		
-		add(djPanel, BorderLayout.EAST);
-		add(scrollPane, BorderLayout.WEST);
-		add(picturePanel, BorderLayout.NORTH);
-		add(buttonPanel, BorderLayout.SOUTH);
-		add(currentPlaying, BorderLayout.CENTER);
 
-		pack();
-		setSize(600,400);
-		setVisible(true);
+		
+		frame.add(djPanel, BorderLayout.EAST);
+		frame.add(picturePanel, BorderLayout.CENTER);
+		frame.add(buttonPanel, BorderLayout.SOUTH);
+	
+		frame.pack();
+		frame.setSize(900,400);
+		frame.setVisible(true);
+		
+		for(Audio audio : playlist)	{
+			delay(audio.getAudioLength() * 200);
+			currentTitle.setText("Now Playing: " + audio.getTitle());
+		}
+		
+		currentTitle.setText("SHIFT OVER");
+			
 	}
 
 	private class createPlaylistListener implements ActionListener
 	{
 		public void actionPerformed(ActionEvent e)
-		{
-			dispose();
-			makeGUI();
+		{ 
+			currentPlaying.add(currentTitle);
+			scrollPane = new JScrollPane(songPanel);
 			
-			//makeGUI();
-			//isPlaying = false;
-			//wmmr.sortMasterList();
-			//wmmr.createPlaylist();
-			//playlist = wmmr.getDjSet1();
-			//makeFrame();
+			djPanel.add(currentDJ);
+			currentDJ.setText(djs.getDjName());
 			
+			frame.add(scrollPane, BorderLayout.WEST);
+			frame.add(currentPlaying, BorderLayout.SOUTH);
+			createPlaylist.setVisible(false);
+			}
 		}
-	}
-
-	public void setCurrentSong(String currentSong) {
-		currentTitle.setText("Now Playing: " + currentSong);	
-	}
 
 	public void displayList(ArrayList<Audio> audioList)	{
-				
+
 		for(Audio audio : audioList)	{
 			System.out.print("Title: " + audio.getTitle() + ". ");				 
-			System.out.println("Audio Length:" + audio.getAudioLength());
-			delay(audio.getAudioLength() * 20);
-			setCurrentSong(audio.getTitle());
-	}
+			System.out.println("Audio Length:" + audio.getAudioLength() + "    ");
+		}
 	}
 
 	public void delay(int milliseconds)	{
